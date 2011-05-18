e227c3c4f36df7fb8752096e41f7ccfe00edb21f
Seiji Sogabe <s.sogabe@gmail.com>
2011-05-18 23:43:41 +0900

    added some test classes.

--- src/test/java/org/jenkinsci/plugins/cloverphp/CloverCoverageParserTest.java
+++ src/test/java/org/jenkinsci/plugins/cloverphp/CloverCoverageParserTest.java
@@ -1,6 +1,6 @@
 package org.jenkinsci.plugins.cloverphp;
 
-import junit.framework.Test;
+import org.junit.Test;
 import junit.framework.TestSuite;
 import junit.framework.TestCase;
 import org.jenkinsci.plugins.cloverphp.results.ClassCoverage;
@@ -16,45 +16,77 @@ import org.jenkinsci.plugins.cloverphp.results.ProjectCoverage;
  * @version 1.0
  */
 public class CloverCoverageParserTest extends TestCase {
+
     public CloverCoverageParserTest(String name) {
         super(name);
     }
 
-    public void setUp() throws Exception {
-        super.setUp();
-    }
-
-    public void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    public void testFailureMode1() throws Exception {
+    @Test
+    public void testTrimPaths_NullResult() {
         try {
-            CloverCoverageParser.parse(null, "");
+            CloverCoverageParser.trimPaths(null, "prefix");
         } catch (NullPointerException e) {
             assertTrue("Expected exception thrown", true);
         }
     }
 
+    @Test
+    public void testTrimPaths_NullPrefix() {
+        ProjectCoverage pc = new ProjectCoverage();
+        ProjectCoverage result = CloverCoverageParser.trimPaths(pc, null);
+        assertEquals(result, pc);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testParse_FileString_NullFile() throws Exception {
+        CloverCoverageParser.parse(null, "");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testParse_File_NullFile() throws Exception {
+        CloverCoverageParser.parse(null);
+    }
+
+    @Test
     public void testParse() throws Exception {
         ProjectCoverage result = CloverCoverageParser.parse(getClass().getResourceAsStream("clover.xml"));
         assertNotNull(result);
         assertEquals(ProjectCoverage.class, result.getClass());
         assertEquals("StringUtilTest", result.getName());
+        assertEquals(1, result.getFiles());        
+        assertEquals(14, result.getNcloc());        
+        assertEquals(16, result.getLoc());
+        assertEquals(1, result.getClasses());
         assertEquals(2, result.getMethods());
+        assertEquals(1, result.getCoveredmethods());
+        assertEquals(2, result.getStatements());
+        assertEquals(1, result.getCoveredstatements());
+        assertEquals(4, result.getElements());
+        assertEquals(2, result.getCoveredelements());
         
         assertEquals(1, result.getFileCoverages().size());
         FileCoverage fileResult = result.getFileCoverages().get(0);
         assertEquals("/var/lib/hudson/jobs/php-sample/workspace/phpsample/src/StringUtil.php", fileResult.getName());
         assertEquals(14, fileResult.getNcloc());        
+        assertEquals(16, fileResult.getLoc());
+        assertEquals(1, fileResult.getClasses());
+        assertEquals(2, fileResult.getMethods());
+        assertEquals(1, fileResult.getCoveredmethods());
+        assertEquals(2, fileResult.getStatements());
+        assertEquals(1, fileResult.getCoveredstatements());
+        assertEquals(4, fileResult.getElements());
+        assertEquals(2, fileResult.getCoveredelements());
+        
         
         assertEquals(1, fileResult.getClassCoverages().size());
         ClassCoverage classResult = fileResult.getClassCoverages().get(0);
         assertEquals("StringUtil", classResult.getName());
+        assertEquals(2, classResult.getMethods());
+        assertEquals(1, classResult.getCoveredmethods());
         assertEquals(2, classResult.getStatements());
+        assertEquals(1, classResult.getCoveredstatements());
+        assertEquals(4, classResult.getElements());
+        assertEquals(2, classResult.getCoveredelements());
     }
 
-    public static Test suite() {
-        return new TestSuite(CloverCoverageParserTest.class);
-    }
 }
--- src/test/java/org/jenkinsci/plugins/cloverphp/RatioTest.java
+++ src/test/java/org/jenkinsci/plugins/cloverphp/RatioTest.java
@@ -1,6 +1,7 @@
 package org.jenkinsci.plugins.cloverphp;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 /**
  * JUnit test for {@link Ratio}
@@ -20,5 +21,168 @@ public class RatioTest extends TestCase {
      */
     public void testParseValue() throws Exception {
         assertRatio(Ratio.create(1,2), 1.0f, 2.0f);
+    }
+
+    /**
+     * Test of toString method, of class Ratio.
+     */
+    @Test
+    public void testToString() {
+        Ratio ratio = Ratio.create(10.0f, 20.2f);
+        assertEquals("10/20.2", ratio.toString());
+    }
+
+    /**
+     * Test of getPercentage1d method, of class Ratio.
+     */
+    @Test
+    public void testGetPercentage1d() {
+        Ratio ratio = Ratio.create(1.0f, 3.0f);
+        assertEquals("33.3", ratio.getPercentage1d());
+    }
+
+    /**
+     * Test of getPercentageStr method, of class Ratio.
+     */
+    @Test
+    public void testGetPercentageStr() {
+        Ratio ratio = Ratio.create(1.0f, 3.0f);
+        assertEquals("33.3%", ratio.getPercentageStr());
+    }
+
+    /**
+     * Test of getPercentageStr method, of class Ratio.
+     */
+    @Test
+    public void testGetPercentageStr_undefined() {
+        Ratio ratio = Ratio.create(1.0f, 0.0f);
+        assertEquals("-", ratio.getPercentageStr());
+        ratio = Ratio.create(1.0f, -5.0f);
+        assertEquals("-", ratio.getPercentageStr());
+    }
+    
+    /**
+     * Test of getPcWidth method, of class Ratio.
+     */
+    @Test
+    public void testGetPcWidth_undefined() {
+        Ratio ratio = Ratio.create(1.0f, -30.0f);
+        assertEquals("-3.3%", ratio.getPcWidth());
+    }
+
+    /**
+     * Test of getPcUncovered method, of class Ratio.
+     */
+    @Test
+    public void testGetPcUncovered() {
+        Ratio ratio = Ratio.create(1.0f, 30.0f);
+        assertEquals("96.7%", ratio.getPcUncovered());
+    }
+
+    /**
+     * Test of getPcCovered method, of class Ratio.
+     */
+    @Test
+    public void testGetPcCovered_plus() {
+        Ratio ratio = Ratio.create(1.0f, 30.0f);
+        assertEquals("3.3%", ratio.getPcCovered());
+    }
+
+    /**
+     * Test of getPcCovered method, of class Ratio.
+     */
+    @Test
+    public void testGetPcCovered_undefined() {
+        Ratio ratio = Ratio.create(1.0f, -30.0f);
+        assertEquals("-", ratio.getPcCovered());
+        ratio = Ratio.create(1.0f, 0.0f);
+        assertEquals("-", ratio.getPcCovered());
+    }
+    
+    /**
+     * Test of getHasData method, of class Ratio.
+     */
+    @Test
+    public void testGetHasData() {
+        Ratio ratio = Ratio.create(1.0f, 30.0f);
+        assertEquals("true", ratio.getHasData());
+        ratio = Ratio.create(1.0f, -30.0f);
+        assertEquals("false", ratio.getHasData());
+    }
+
+    /**
+     * Test of getPercentage method, of class Ratio.
+     */
+    @Test
+    public void testGetPercentage() {
+        Ratio ratio = Ratio.create(1.0f, 30.0f);
+        assertEquals(3, ratio.getPercentage());
+        ratio = Ratio.create(1.0f, 40.0f);
+        assertEquals(3, ratio.getPercentage());
+    }
+
+    /**
+     * Test of getPercentageFloat method, of class Ratio.
+     */
+    @Test
+    public void testGetPercentageFloat_0() {
+        Ratio ratio = Ratio.create(1.0f, 0.0f);
+        assertTrue(Float.compare(0.0f, ratio.getPercentageFloat()) == 0);
+    }
+
+    /**
+     * Test of getPercentageFloat method, of class Ratio.
+     */
+    @Test
+    public void testGetPercentageFloat_100() {
+        Ratio ratio = Ratio.create(10.0f, 10.0f);
+        assertTrue(Float.compare(100.0f, ratio.getPercentageFloat()) == 0);
+    }
+
+    /**
+     * Test of getPercentageFloat method, of class Ratio.
+     */
+    @Test
+    public void testGetPercentageFloat() {
+        Ratio ratio = Ratio.create(5.0f, 10.0f);
+        assertTrue(Float.compare(50.0f, ratio.getPercentageFloat()) == 0);
+    }
+
+    /**
+     * Test of equals method, of class Ratio.
+     */
+    @Test
+    public void testEquals_same() {
+        Ratio ratio = Ratio.create(5.0f, 10.0f);
+        assertTrue(ratio.equals(ratio));
+    }
+
+    /**
+     * Test of equals method, of class Ratio.
+     */
+    @Test
+    public void testEquals() {
+        Ratio ratio = Ratio.create(5.0f, 10.0f);
+        Ratio another = Ratio.create(5.0f, 10.0f);
+        assertTrue(ratio.equals(another));
+    }
+    
+    /**
+     * Test of equals method, of class Ratio.
+     */
+    @Test
+    public void testEquals_TargetNull() {
+        Ratio ratio = Ratio.create(5.0f, 10.0f);
+        assertFalse(ratio.equals(null));
+        assertFalse(ratio.equals(Integer.class));
+    }
+    
+    /**
+     * Test of create method, of class Ratio.
+     */
+    @Test
+    public void testCreate() {
+        Ratio ratio = Ratio.create(5.0f, 10.0f);
+        assertNotNull(ratio);
     }
 }
